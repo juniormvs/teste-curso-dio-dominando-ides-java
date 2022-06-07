a10fd4d1f21f1bf2f4f455973182f6731c3fed5c
juniormvs <juniormvs@hotmail.com>
2022-06-07 17:47:04 -0300

    second commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -11,8 +11,7 @@ public class PrimeiroPrograma {
 		System.out.println(gato);
 		System.out.println(livro);	
 		
-		/*
-		int a =2;
+		/*int a =2;
 		int b =3;
 		System.out.println("Hello world! " + (a+b));
 		*/
